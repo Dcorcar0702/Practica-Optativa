3d9cd2188ecd5dc2dc2b37ac914b638dfaeb4df6
David Córdoba Carreto <dcorcar0702@g.educaand.es>
2026-09-24 10:00:47 +0200

    Add files via upload

---
+++ formulario/formulario.js
@@ -0,0 +1,5 @@
+  document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const nombre = document.getElementById('nombreInput').value;
+    document.getElementById('salida').textContent = '¡Hola, ' + nombre + '!';
+  });
